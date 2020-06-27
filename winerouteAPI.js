14ab386efa6ad3f04ebabd0c8fa9eab3062f1a24
Manuel Rocha <9170249@esmad.ipp.pt>
2020-06-28 00:31:34 +0100

    fixing cors

--- winerouteAPI.js
+++ winerouteAPI.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({
     extended: false
