4921ee16935dfd833948b7881810ee1c603e4608
Manuel Rocha <9170249@esmad.ipp.pt>
2020-06-27 22:56:42 +0100

    trying to fix

--- winerouteAPI.js
+++ winerouteAPI.js
@@ -1,43 +1,21 @@
 const express = require('express');
-const cors = require('cors');
-const session = require('express-session');
-//para usar o req.body, body parsing
 const bodyParser = require('body-parser');
 
 const app = express();
-
-app.use(cors({
-    credentials: true,
-    origin: true
-}))
-
 app.use(express.json());
-
-var host = process.env.HOST || 'localhost';
-
-const port = process.env.PORT || 8080;
-
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
-
-
-
-//Queremos JSON
 app.use(bodyParser.json());
 
-//rotas têm de vir no fim, senão há erros
+
+const cors = require('cors');
+app.use(cors())
+
+//Routes
 const userRouter = require("./routes/users.router");
 const routesRouter = require("./routes/routes.router");
 const commentsRouter = require("./routes/comments.router");
@@ -50,13 +28,4 @@ app.use(commentsRouter);
 app.use(poisRouter);
 app.use(categoriesRouter);
 
-/* var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    //requireHeader: ['origin', 'x-requested-with'],
-    //removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-}); */
-
-app.listen(port, () => console.log(`We're live on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
